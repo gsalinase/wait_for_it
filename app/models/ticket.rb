7e09db50589092c1bf1204b93021705fab8d16b7
Gonzalo Salinas <gsalinase91@gmail.com>
2020-08-16 22:20:56 -0400

    Add ticket model

---
+++ app/models/ticket.rb
@@ -0,0 +1,4 @@
+class Ticket < ApplicationRecord
+  belongs_to :company
+  belongs_to :customer
+end
---
+++ db/migrate/20200817020611_create_tickets.rb
@@ -0,0 +1,13 @@
+class CreateTickets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tickets do |t|
+      t.references :company, foreign_key: true
+      t.references :customer, foreign_key: true
+      t.integer :ticket_number
+      t.boolean :priority
+      t.integer :state
+
+      t.timestamps
+    end
+  end
+end
