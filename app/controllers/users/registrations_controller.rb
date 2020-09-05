7c601cd08d4c99e787d1e57443ec0535df52e426
Gonzalo Salinas <gsalinase91@gmail.com>
2020-09-05 19:30:31 -0400

    add update controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
   before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   def new
@@ -19,18 +19,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
     UserMailer.with(user: resource).welcome_email.deliver_now if resource.save
     render_resource(resource)
-    # super
   end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+    super
+  end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    super
+  end
 
   # DELETE /resource
   # def destroy
@@ -52,11 +51,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone, :role, :birthday, :critical_role])
   end
-  
+
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone, :role, :birthday, :critical_role])
+  end
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
