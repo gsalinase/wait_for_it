32becb7ec8f0527dc52609329d54aad06d562adf
Gonzalo Salinas <gsalinase91@gmail.com>
2020-09-05 21:44:58 -0400

    add ENV to production

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module WaitForIt
 
     config.action_mailer.delivery_method = :sendgrid_actionmailer
     config.action_mailer.sendgrid_actionmailer_settings = {
-      api_key: Rails.application.credentials.sendgrid_api_key,
+      api_key: (ENV['SENDGRID_API_KEY'] || Rails.application.credentials.sendgrid_api_key),
       raise_delivery_errors: true
     }
   end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, Rails.application.credentials.app_id, Rails.application.credentials.app_secret,
+  config.omniauth  :facebook, (ENV['FACEBOOK_APP_ID'] || Rails.application.credentials.app_id), (ENV['FACEBOOK_APP_SECRET'] || Rails.application.credentials.app_secret),
                   strategy_class: OmniAuth::Strategies::Facebook
 
   # ==> Warden configuration
@@ -310,7 +310,7 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.jwt_secret
+    jwt.secret = (ENV['JWT_SECRET'] || Rails.application.credentials.jwt_secret)
     warn('warning: jwt.secret can not be nil') if jwt.secret.nil?
     #  You need to tell which requests will dispatch tokens for the user that has been previously
     #  authenticated (usually through some other warden strategy, such as one requiring username and email parameters).
